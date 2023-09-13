6de8c9ffb46bd90605dad946548aa219e98f8673
Nathan Dygant <nathan.dygant@gmail.com>
2023-09-13 18:52:21 +0200

    ADding some basic temperature reading functionality

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("test 2")
+from jobs.utils.temperature import Temperature
 
+if __name__ == "__main__":
+    temperature_reader = Temperature()
+
+    print(f"Current temperature is: {temperature_reader.read_temp()}°C")

d3b6cea6b5e982fe06578e0943bc38eef280b65e
Nathan Dygant <nathan.dygant@gmail.com>
2023-09-14 10:54:36 +0200

    Adding humidity and presssure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from jobs.utils.temperature import Temperature
+from jobs.utils.humidity import Humidity
 
 if __name__ == "__main__":
     temperature_reader = Temperature()
+    humidity_reader = Humidity()
+
+    pressure, humidity = humidity_reader.get_readings()
 
     print(f"Current temperature is: {temperature_reader.read_temp()}°C")
+    print(f"Current humidity is: {humidity}%")
+    print(f"Current pressure is: {pressure}hPa")
